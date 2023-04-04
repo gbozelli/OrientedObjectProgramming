04e8c57b5a85868a9fe26c61919402143551c42a
Zobelli <bozelli222gabriel@gmail.com>
2023-04-03 21:57:22 -0300

    basics concepts of classes in Java

---
+++ containers.java
@@ -0,0 +1,20 @@
+class Lesson3{
+    public static void main(String[] args){
+    }
+}
+
+class container{
+    int capacity = 100;
+    int current;
+    public void add(int quantity){
+        if(this.current+quantity<capacity)
+    }
+    public void move(int quantity, container destiny){
+        if(this.current-quantity>0 && destiny.current+quantity<capacity){
+            this.
+        }
+    }
+    public void remove(int quantity){
+        if(this.current+quantity<capacity)
+    }
+}
